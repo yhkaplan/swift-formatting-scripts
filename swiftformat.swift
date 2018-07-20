6a61b54245fec218247b3fdecbea5fd693c5778a
Joshua Kaplan <yhkaplan@gmail.com>
2018-07-21 00:20:10 +0900

    Add recursive swiftlint use

--- swiftformat.swift
+++ swiftformat.swift
@@ -2,17 +2,42 @@
 
 import Foundation
 
+let launchPath = "/usr/bin/env"
+
 @discardableResult
 func shell(_ command: String) -> Int32 {
     let args = command.components(separatedBy: " ")
 
     let process = Process()
-    process.launchPath = "/usr/bin/env"
+    process.launchPath = launchPath
     process.arguments = args
     process.launch()
     process.waitUntilExit()
 
     return process.terminationStatus
+}
+
+func pipeOut(_ command: String) -> (Process, Pipe) {
+    let args = command.components(separatedBy: " ")
+    let pipe = Pipe()
+    let process = Process()
+
+    process.launchPath = launchPath
+    process.arguments = args
+    process.standardOutput = pipe
+
+    return (process: process, pipe: pipe)
+}
+
+func pipeIn(_ command: String, pipe: Pipe) -> Process {
+    let args = command.components(separatedBy: " ")
+    let process = Process()
+
+    process.launchPath = launchPath
+    process.arguments = args
+    process.standardInput = pipe
+
+    return process
 }
 
 let disabledRules: [String] = [
@@ -32,6 +57,8 @@ let disabledRules: [String] = [
 
 var filePath: String?
 var configFile: String?
+var shouldRunSwiftlint = true
+var shouldRunSwiftFormat = true
 
 let args = ProcessInfo.processInfo.arguments
 args.enumerated().forEach { index, arg in
@@ -40,21 +67,49 @@ args.enumerated().forEach { index, arg in
         filePath = args[index + 1]
     case "-p":
         filePath = args[index + 1]
+
     case "--config":
         configFile = args[index + 1]
     case "-c":
         configFile = args[index + 1]
+
+    case "--swiftlint-only":
+        shouldRunSwiftFormat = false
+    case "-sl":
+        shouldRunSwiftFormat = false
+
+    case "--swiftformat-only":
+        shouldRunSwiftlint = false
+    case "-sf":
+        shouldRunSwiftlint = false
+
     default:
         break
     }
 }
 
-guard let filePath = filePath else { print("Missing --path/-p flag"); exit(1) }
+guard let filePath = filePath else {
+    print("Missing --path/-p flag"); exit(1)
+}
 
-print("Running Swiftformat")
-shell("swiftformat --disable \(disabledRules.joined(separator: ",")) \(filePath)")
+if shouldRunSwiftFormat {
+    print("Running Swiftformat")
+    shell("swiftformat --disable \(disabledRules.joined(separator: ",")) \(filePath)")
+}
 
-guard let configFile = configFile else { print("Missing --config/-c flag"); exit(1) }
+guard shouldRunSwiftlint else { exit(1) }
+
+guard let configFile = configFile else {
+    print("Missing --config/-c flag"); exit(1)
+}
 
 print("Running Swiftlint Autocorrect")
-shell("swiftlint autocorrect --path \(filePath) --config \(configFile)")
+// Must call Swiftlint recursively on each swift file in filePath
+let (process1, pipe) = pipeOut("find \(filePath) -type f -print0")//-name \\*.swift
+let process2 = pipeIn("xargs -0 swiftlint autocorrect --config \(configFile) --path", pipe: pipe)
+
+let outputPipe = Pipe()
+process2.standardOutput = outputPipe
+process1.launch()
+process2.launch()
+process2.waitUntilExit()
